6d60caffc65e4ef6b9328d7ad1a25939f72d6503
Aarti Jangid <jangid.aarti08@gmail.com>
2020-04-09 22:25:51 +0200

    twitter handler added

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID
     ], 
     callback: {
